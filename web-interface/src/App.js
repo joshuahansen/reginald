07d3656216fff5291435cf8ea6533912f263ff62
Lohgan Nash <lohgannash@gmail.com>
2018-09-13 13:16:59 +1000

    Replaced default page with simple AppBar

--- web-interface/src/App.js
+++ web-interface/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const muiTheme = createMuiTheme ({
+    palette: {
+        "primary1Color": "#e60228",
+        "primary2Color": "#d32f2f",
+        "accent1Color": "#448aff"
+    },
+});
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <div>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Reginald Management System
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+
     );
   }
 }
