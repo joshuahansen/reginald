505d5fc8d628a38ed1173e9612607524c25040a3
Lohgan Nash <lohgannash@gmail.com>
2018-10-08 15:39:56 +1100

    Began work on facial recognition form

--- web-interface/src/App.js
+++ web-interface/src/App.js
@@ -8,6 +8,13 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 const theme = createMuiTheme ({
     palette: {
@@ -18,20 +25,79 @@ const theme = createMuiTheme ({
 
 class App extends Component {
 
+  state = {
+    open: false,
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+  handleSubmit() {
+
+    console.log("Test");
+
+  }
+
+
   render() {
+
     return (
       <MuiThemeProvider theme={theme}>
         <div>
           <AppBar position="static" color="primary">
             <Toolbar>
-              <IconButton color="inherit" aria-label="Menu">
+              <IconButton onClick={this.toggleDrawer('open', true)} color="inherit" aria-label="Menu">
                 <MenuIcon />
               </IconButton>
+              <Drawer className="drawer" open={this.state.open} onClose={this.toggleDrawer('open', false)}>
+                <div
+                  tabIndex={0}
+                  role="button"
+                  onClick={this.toggleDrawer('open', false)}
+                  onKeyDown={this.toggleDrawer('open', false)}
+                >
+
+                  <div>
+                    <MenuItem>Register Face</MenuItem>
+                    <MenuItem>Menu Item 2</MenuItem>
+                    <MenuItem>Menu Item 3</MenuItem>
+                    <MenuItem>Menu Item 4</MenuItem>
+                  </div>
+                </div>
+              </Drawer>
               <Typography variant="title" color="inherit">
                 Reginald Management System
               </Typography>
             </Toolbar>
           </AppBar>
+
+          <h1 className="register-h1">
+            Register New Face
+          </h1>
+
+          <div className="recognition">
+            <form className="recognition-form" onSubmit={this.handleSubmit}>
+              <TextField
+                id="standard-dense"
+                label="Name"
+                margin="dense"
+              />
+              <div className="recognition-button">
+                <Button type="submit" variant="contained" color="primary">
+                  Register
+                </Button>
+              </div>
+            </form>
+          </div>
+
         </div>
       </MuiThemeProvider>
     );
